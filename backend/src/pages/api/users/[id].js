c8703582f5313a4f32b821f9ac8374828a32bee3
David Zhang <zdavid4525@gmail.com>
2024-10-11 14:33:18 -0400

    updated update user details error handling

--- backend/src/pages/api/users/[id].js
+++ backend/src/pages/api/users/[id].js
@@ -8,26 +8,50 @@ export default async function handler(req, res) {
     id = Number.parseInt(id);
     const decodedJWT = verifyAndDecodeJWT(req, id);
     if (!decodedJWT) {
-      res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: "Unauthorized" });
     }
 
     // the following fields will be null there is no change; otherwise, they will be a string w updated value.
     const { newUsername, newPassword, newFirstName, newLastName, newEmail, newPhoneNumber } = req.body;
     if (newPassword) {
       const newPasswordHash = await bcrypt.hash(newPassword, 10);
-      users_db.updatePasswordHashById(id, newPasswordHash);        
+      await users_db.updatePasswordHashById(id, newPasswordHash);        
     }
-    if (newFirstName)   users_db.updateFirstNameById(id, newFirstName);
-    if (newLastName)    users_db.updateLastNameById(id, newLastName);
+    if (newFirstName)   await users_db.updateFirstNameById(id, newFirstName);
+    if (newLastName)    await users_db.updateLastNameById(id, newLastName);
+    
+    const errs = [];
+    try {
+      if (newUsername)    await users_db.updateUsernameById(id, newUsername);
+    } catch (e) {
+      const err = new Object();
+      err[errs.length] = `A user with username ${newUsername} already exists.`;
+      errs.push(err);
+    }
 
-    // the below cannot have duplicates. the UPDATE may fail in DB - error handling required
-    if (newUsername)    users_db.updateUsernameById(id, newUsername);
-    if (newEmail)       users_db.updateEmailById(id, newEmail);
-    if (newPhoneNumber) users_db.updatePhoneNumberById(id, newPhoneNumber);
+    try {
+      if (newEmail)       await users_db.updateEmailById(id, newEmail);
+    } catch (e) {
+      const err = new Object();
+      err[errs.length] = `A user with email ${newEmail} already exists.`;
+      errs.push(err);
+    }
+
+    try {
+      if (newPhoneNumber) await users_db.updatePhoneNumberById(id, newPhoneNumber);
+    } catch (e) {
+      const err = new Object();
+      err[errs.length] = `A user with phone number ${newPhoneNumber} already exists.`;
+      errs.push(err);
+    }
 
     const jwt = getJWT(await users_db.getUserById(id), 15);
-    res.status(200).json({ token: jwt });  // return JWT with updated user details
+    if (errs.length === 0) {
+      return res.status(200).json({ token: jwt });  // return JWT with updated user details
+    } else {
+      return res.status(409).json({ errors: errs, token: jwt });
+    }
   } else {
-    res.status(405).json({ error: "Method not allowed." });
+    return res.status(405).json({ error: "Method not allowed." });
   }
 }
--- backend/src/pages/api/users/profilePic.js
+++ backend/src/pages/api/users/profilePic.js
@@ -2,6 +2,7 @@ import { verifyAndDecodeJWT } from '@/service/jwt';
 const formidable = require("formidable");
 
 // ref: https://gist.github.com/agmm/da47a027f3d73870020a5102388dd820 - ref for frontend
+// too lazy to test this... to test we need partially implemented frontend
 export default async function handler(req, res) {
   if (req.method === "POST") {
     let { id } = req.query;
