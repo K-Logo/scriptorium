6d22c01704ce895dc55748f723fb68f88a1fb6fc
David Zhang <zdavid4525@gmail.com>
2024-11-03 15:01:31 -0500

    added avatar selections and GET endpoint to retrieve all possible avatars (for FE to ref)

---
+++ backend/src/pages/api/users/avatars.js
@@ -0,0 +1,16 @@
+import { verifyAndDecodeJWTNoId } from '@/service/jwt';
+const fs = require('fs');
+
+export default async function getAvatars(req, res) {
+  if (req.method === "GET") {
+    const decodedJWT = verifyAndDecodeJWTNoId(req);
+    if (!decodedJWT) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const files = fs.readdirSync('./public/avatars')
+                    .map((file) => "localhost:3000/avatars/" + file);
+    res.status(200).json(files);
+  } else {
+    return res.status(405).json({ error: "Method not allowed." });
+  }
+}
