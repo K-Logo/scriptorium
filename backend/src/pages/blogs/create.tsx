bf2699eecef2cb387b4b4ba91538981feb7ee47c
Kirill Logoveev <k.logoveev@mail.utoronto.ca>
2024-11-26 02:10:09 -0500

    implemented a back button

--- backend/src/pages/blogs/create.tsx
+++ backend/src/pages/blogs/create.tsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { UserContext } from "../../contexts/user";
+import Link from "next/link";
 
 export default function CreateBlog() {
   const [title, setTitle] = useState<string>("");
@@ -51,7 +52,18 @@ export default function CreateBlog() {
 
   return (
     <div className="p-6 max-w-4xl mx-auto min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Create a New Blog</h1>
+      <div className="flex items-left mb-4 justify-center">
+        <Link href='/blogs'>
+          <button
+            className="absolute left-[4.5vw] px-4 py-2 bg-gray-700 text-white rounded-md hover:bg-gray-800"
+          >
+            Back
+          </button>
+        </Link>
+
+        <h1 className="text-2xl font-bold text-center">Create a New Blog</h1>
+
+      </div>      
       <div className="mb-4">
         <label className="block text-sm font-medium mb-2">Title</label>
         <input
