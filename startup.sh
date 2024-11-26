69b3925f96a563fe77bb66a1f9ff3f1f308d07d0
David Zhang <zdavid4525@gmail.com>
2024-11-26 18:23:42 -0500

    fixed docker-exec

--- startup.sh
+++ startup.sh
@@ -2,6 +2,15 @@
 
 cd backend
 
+echo "DATABASE_URL="postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public"
+SECRET_KEY=SECRETKEY
+SALTROUNDS=10" > .env
+
+cd src/pages/api/exec/sandbox
+docker build -t sandbox .
+
+cd ../../../../..
+
 npm i
 npm i prisma @prisma/client @prisma/studio
 npx prisma generate
